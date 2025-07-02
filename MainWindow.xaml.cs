23acdafae25966ee3baa185347234c69fb917c6b
John Eric Magat <johnericmagat@gmail.com>
2025-07-02 17:18:04 +0800

    Added functions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,11 @@
-﻿using System.Text;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Win32;
+using System.Globalization;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DataTable = System.Data.DataTable;
+using Window = System.Windows.Window;
 
 namespace BachelorsPhSalesProcessor
 {
@@ -16,9 +14,76 @@ namespace BachelorsPhSalesProcessor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string fileLocation;
+        private string fileName;
+
+        DataTable salesCSV;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Process()
+        {
+            GetSalesCSV();
+        }
+
+        private void GetSalesCSV()
+        {
+            salesCSV = new DataTable();
+
+            using var reader = new StreamReader(fileLocation);
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                IgnoreBlankLines = true,
+                DetectColumnCountChanges = true
+            });
+
+            using var dr = new CsvDataReader(csv);
+            salesCSV.Load(dr);
+        }
+
+
+
+
+
+        private void BtnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Start processing?", "PROCESS",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Process();
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Close this application?", "CLOSE",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.DefaultExt = ".csv";
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;
+                TxtFilePath.Text = filename;
+                fileLocation = filename;
+                fileName = filename.Split('\\')[filename.Split('\\').Length - 1];
+            }
         }
     }
 }
