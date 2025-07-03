3f6ddec2c8f446ba042673eb9271e50fe3c8d13e
John Eric Magat <johnericmagat@gmail.com>
2025-07-03 10:31:55 +0800

    Helpers

---
+++ Helper/WriteLogFileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace BachelorsPhSalesProcessor.Helper
+{
+    public static class WriteLogFileHelper
+    {
+        public static void WriteLogFile(string message)
+        {
+            var filename = AppDomain.CurrentDomain.BaseDirectory + "\\logs\\" + DateTime.Now.Year.ToString() + DateTime.Now.Month + DateTime.Now.Day + ".txt";
+            Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\logs\\");
+            FileStream file = new FileStream(filename, FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file); writer.WriteLine(DateTime.Now); writer.WriteLine(message);
+            writer.WriteLine("\n");
+            writer.Close();
+            writer.Dispose();
+            file.Close();
+            file.Dispose();
+        }
+    }
+}
